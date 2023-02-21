a253ff94d4e94eba9fc132f6afb2cb5ad919a3cd
行客 远 <yanzhe_zhang@qq.com>
2023-02-21 14:16:46 +0000

    initial

---
+++ Application/main.cpp
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "ui_main.h"
+
+main::main(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::main)
+{
+    ui->setupUi(this);
+}
+
+main::~main()
+{
+    delete ui;
+}
+
